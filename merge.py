3d0c6d1ef2519d14756d0dd4cb6a0be172c38ad3
Tang Bo Hao <tech@btang.cn>
2025-05-20 21:40:01 +0800

    refactor:  merge.py - reorganize and expand the list of essential Cadence repositories, categorizing them into Docs, Main Repos, and Ecosystem Repos.

--- merge.py
+++ merge.py
@@ -8,26 +8,11 @@ from pathlib import Path
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, 'scraped_docs')
 
-ESSENTIALS = [
-  # Docs
-  'developers_flow_com',
-
-  # GitHub Repos
-  'github_com_onflow_cadence_lang_org',
-  'github_com_onflow_flips',
-  'github_com_onflow_flow_core_contracts',
-  'github_com_onflow_flow_ft',
-  'github_com_onflow_flow_nft',
-  'github_com_onflow_nft_storefront',
-  'github_com_onflow_flow_evm_bridge',
-  'github_com_dapperlabs_nba_smart_contracts',
-  'github_com_dapperlabs_nfl_smart_contracts',
-  'github_com_outblock_frw_scripts',
-]
-
 CADENCE_DOCS = [
-    # GitHub Repos
+    # GitHub Docs
     'github_com_onflow_cadence_lang_org',
+
+    # Main Repos
     'github_com_onflow_flow_core_contracts',
     'github_com_onflow_flow_ft',
     'github_com_onflow_flow_nft',
@@ -36,7 +21,56 @@ CADENCE_DOCS = [
     'github_com_dapperlabs_nba_smart_contracts',
     'github_com_dapperlabs_nfl_smart_contracts',
     'github_com_outblock_frw_scripts',
+    
+    # Ecosystem Repos
+    'github_com_ballerz_nft_ballerz',
+    'github_com_dappcoderr_cadence_fun',
+    'github_com_flowtyio_avataaars',
+    'github_com_flowtyio_capability_cache',
+    'github_com_flowtyio_fantastec',
+    'github_com_flowtyio_flow_contracts',
+    'github_com_flowtyio_flow_raffle',
+    'github_com_flowtyio_flowty_drops',
+    'github_com_flowtyio_flowty_wrapped',
+    'github_com_flowtyio_fungible_token_router',
+    'github_com_flowtyio_lost_and_found',
+    'github_com_flowtyio_nft_catalog',
+    'github_com_flowtyio_unblocked',
+    'github_com_graffle_cadence_1_0_contracts',
+    'github_com_incrementfi_liquid_staking',
+    'github_com_incrementfi_money_market',
+    'github_com_incrementfi_oracle',
+    'github_com_incrementfi_swap',
+    'github_com_itahand_coa_example',
+    'github_com_itahand_cadence_arch_example',
+    'github_com_austinkline_jimbo',
+    'github_com_austinkline_managed_account',
+    'github_com_blocto_bloctoswap_contracts',
+    'github_com_blocto_blt_contracts',
+    'github_com_blocto_flow_transactions',
+    'github_com_blocto_revv_contracts',
+    'github_com_bluesign_cadenceupgrader',
+    'github_com_crash13override_flovatar',
+    'github_com_dapperlabs_studio_platform_smart_contracts',
+    'github_com_emerald_dao_emerald_academy_v2',
+    'github_com_emerald_dao_emerald_id',
+    'github_com_emerald_dao_float',
+    'github_com_emerald_dao_project_toucans_v2',
+    'github_com_fixes_world_token_list',
+    'github_com_fixes_world_fixes',
+    'github_com_onflow_bridged_usdc',
+    'github_com_onflow_cadence',
+    'github_com_onflow_flips',
+    'github_com_onflow_nft_catalog',
+    'github_com_onflow_random_coin_toss',
+    'github_com_onflow_hybrid_custody',
 ]
+
+# concat CADENCE_DOCS to ESSENTIALS
+ESSENTIALS = [
+  # Docs
+  'developers_flow_com',
+].extend(CADENCE_DOCS)
 
 OUTPUT_DIR = os.path.join(BASE_DIR, 'merged_docs')
 
